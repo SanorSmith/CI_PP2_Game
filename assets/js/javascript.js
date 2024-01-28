a16ca63bc5d7f5369435c55e497671e2e6c4dfd0
SanorSharmen <sanorsha@hotmail.com>
2024-01-28 19:29:12 +0100

    Transfering code from rules-function.js to javascript.js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -164,3 +164,35 @@ twoPlayersButton.addEventListener("click", () => {
    startMessages.innerHTML = "Click start game to play";
    startMessages.style.display = "none";
 });
+
+
+
+
+//Rulls function for the game 
+
+let boardState = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let onePlayerMode = false;
+let gameWon = false;
+
+// Function to handle cell click
+function handleCellClick(index) {
+    if (boardState[index] === "" && !gameWon) {
+        boardState[index] = currentPlayer;
+        renderBoard();         
+    }
+}
+
+// Function to render the Tic Tac Toe board
+const board = document.getElementById("board");
+
+export function renderBoard() {
+    board.innerHTML = "";
+    boardState.forEach((cell, index) => {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("cell");
+        cellElement.innerText = cell;
+        cellElement.addEventListener("click", () => handleCellClick(index));
+        board.appendChild(cellElement);
+    });
+}
